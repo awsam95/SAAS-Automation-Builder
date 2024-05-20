20c94ae4687bf81bc676cdd130030176a1b95bdd
Awsam <awsam.haitham@outlook.com>
2024-05-20 18:53:53 +0300

    feat: reforming boxs

--- src/components/global/container-scroll-animation.tsx
+++ src/components/global/container-scroll-animation.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { useScroll, useTransform, motion } from 'framer-motion'
 import Image from 'next/image'
 
@@ -8,13 +8,14 @@ export const ContainerScroll = ({
 }: {
   titleComponent: string | React.ReactNode
 }) => {
-  const containerRef = useRef<any>(null)
+  const containerRef = useRef(null)
   const { scrollYProgress } = useScroll({
     target: containerRef,
+    offset: ["start start", "end end"]
   })
-  const [isMobile, setIsMobile] = React.useState(false)
+  const [isMobile, setIsMobile] = useState(false)
 
-  React.useEffect(() => {
+  useEffect(() => {
     const checkMobile = () => {
       setIsMobile(window.innerWidth <= 768)
     }
@@ -25,12 +26,10 @@ export const ContainerScroll = ({
     }
   }, [])
 
-  const scaleDimensions = () => {
-    return isMobile ? [0.7, 0.9] : [1.05, 1]
-  }
+  const scaleDimensions = isMobile ? [0.7, 0.9] : [1.05, 1]
 
   const rotate = useTransform(scrollYProgress, [0, 1], [20, 0])
-  const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions())
+  const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions)
   const translate = useTransform(scrollYProgress, [0, 1], [0, -100])
 
   return (
@@ -64,7 +63,7 @@ export const Header = ({ translate, titleComponent }: any) => {
       style={{
         translateY: translate,
       }}
-      className="div max-w-5xl mx-auto text-center"
+      className="max-w-5xl mx-auto text-center"
     >
       {titleComponent}
     </motion.div>
@@ -83,14 +82,16 @@ export const Card = ({
   return (
     <motion.div
       style={{
-        rotateX: rotate, // rotate in X-axis
+        rotateX: rotate,
         scale,
+        translateY: translate,
         boxShadow:
           '0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003',
       }}
-      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full  p-6 bg-[#222222] rounded-[30px] shadow-2xl"
+      transition={{ ease: "easeInOut", duration: 0.5 }}
+      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full p-6 bg-[#222222] rounded-[30px] shadow-2xl"
     >
-      <div className="bg-gray-100 h-full w-full rounded-2xl  gap-4 overflow-hidden p-4 transition-all ">
+      <div className="bg-gray-100 h-full w-full rounded-2xl gap-4 overflow-hidden p-4 transition-all">
         <Image
           src="/temp-banner.png"
           fill
